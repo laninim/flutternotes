cf4058c2250034fec742c644dd3ef698c67dde60
Mauro <Lanini>
2023-08-28 20:39:57 +0200

    add appbar to createnote

--- lib/application/screen/addnotescreen/create_note_screen.dart
+++ lib/application/screen/addnotescreen/create_note_screen.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 
+//create note show note edit note
+
 class CreateNoteScreen extends StatelessWidget {
   const CreateNoteScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
+      appBar: AppBar(
+        title: const Text("Note1"),
+      actions: const [
+        Icon(Icons.save)
+      ],
+      ),
         body: WillPopScope(
           onWillPop: () async {
             return true; 
